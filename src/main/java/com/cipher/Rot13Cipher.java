fee22234518d38ec543528acd3f58f82f1a8e09d
Claudiu Duneata <cduneata@gmail.com>
2021-10-08 23:26:23 +0300

    Add method to decrypt rot13  string

--- src/main/java/com/cipher/Rot13Cipher.java
+++ src/main/java/com/cipher/Rot13Cipher.java
@@ -1,5 +1,22 @@
 package com.cipher;public class Rot13Cipher {
     /**
+     * Decrypt string
+     * When an encrypted text is provided the function returns the decrypted string
+     * @param text encrypted string
+     * @return decrypted string
+     */
+    public String decrypt(String text) {
+        String decrypted = "";
+        for(int i = 0; i < text.length(); i++) {
+            if(Character.isLetter(text.charAt(i)))
+                decrypted += String.valueOf(decryptLetter(text.toLowerCase().charAt(i)));
+            else
+                decrypted += text.charAt(i);
+        }
+        return decrypted;
+    }
+
+    /**
      * Initialize map for rot13cipher
      * For each key add alphabet letter and for value add rot 13 equivalent letter
      */
