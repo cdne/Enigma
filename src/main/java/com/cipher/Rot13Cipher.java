dbb2a2de389576b2f32f5accdb91e0f449c98cdd
Claudiu Duneata <cduneata@gmail.com>
2021-10-08 23:30:23 +0300

    Remove Locale.root from toLowerCase method

--- src/main/java/com/cipher/Rot13Cipher.java
+++ src/main/java/com/cipher/Rot13Cipher.java
@@ -80,8 +80,8 @@ public class Rot13Cipher implements Cipher {
      * For each key add alphabet letter and for value add rot 13 equivalent letter
      */
     private void initializeMap(){
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase(Locale.ROOT);
-        String rot13Alphabet = "NOPQRSTUVWXYZABCDEFGHIJKLM".toLowerCase(Locale.ROOT);
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toLowerCase();
+        String rot13Alphabet = "NOPQRSTUVWXYZABCDEFGHIJKLM".toLowerCase();
         for(int i = 0; i < alphabet.length();i++) {
             map.put(alphabet.charAt(i), rot13Alphabet.charAt(i));
         }
