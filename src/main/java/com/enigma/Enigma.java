d3ffb5f9a0c024ee7c29905a6b828e851c5fc043
Claudiu Duneata <cduneata@gmail.com>
2021-10-08 23:32:17 +0300

    Add object to call Rot13Cipher class

--- src/main/java/com/enigma/Enigma.java
+++ src/main/java/com/enigma/Enigma.java
@@ -1,4 +1,16 @@
 package com.enigma;
 
+import com.cipher.Cipher;
+import com.cipher.CipherFactory;
+import com.cipher.Rot13Cipher;
+
 public class Enigma {
+    public static void main(String[] args) {
+        System.out.println("Test");
+
+        Rot13Cipher cipher = new Rot13Cipher();
+        System.out.println(cipher.decrypt("qrsraqgurrnfgjnyybsgurpnfgyr"));
+
+
+    }
 }
